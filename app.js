7b4d6f4d475079995662559c4cfa3661da86b86a
Migueleng <142732869+Migueleng@users.noreply.github.com>
2023-10-26 21:26:18 -0700

    Update app.js

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.get("/html", function(req,res){
         //res.type('application/json');
         //res.json({'name': 'S. Legah'});
         res.send({
-		'name': 'Landmark Technologies',
+		'name': 'Miguel Technologies',
 		'technology': 'DevOps',
 		'contact' : '+14372152483',
 		'email': 'info@mylandmarktech.com'
